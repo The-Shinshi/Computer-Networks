2591e745354284fa52b5ea668b7631374a93876a
The-Shinshi <155411903+The-Shinshi@users.noreply.github.com>
2024-12-08 02:32:57 +0530

    Update UDPServer.java

--- p10/UDPServer.java
+++ p10/UDPServer.java
@@ -1,26 +1,31 @@
 import java.io.*;
 import java.net.*;
-public class UDPServer
-{
-	public static void main(String[] args)
-{
-DatagramSocket skt=null;
-try
-{
-System.out.println("server is started");
-skt=new DatagramSocket(6788);
-byte[] buffer = new byte[1000]; while(true)
-{
-DatagramPacket request = new DatagramPacket(buffer,buffer.length); skt.receive(request);
-String[] message = (new String(request.getData())).split(" ");
-byte[] sendMsg = (message[1].toUpperCase()+ " from server to client").getBytes();
-DatagramPacket reply = new DatagramPacket(sendMsg,sendMsg.length,request.getAddress(),request.getPort());
-skt.send(reply);
-}
-}
-catch(Exception ex)
-{
-System.out.println(ex.getMessage());
-}
-}
+
+public class UDPServer {
+    public static void main(String[] args) {
+        DatagramSocket socket = null;
+        try {
+            System.out.println("Server is started");
+            socket = new DatagramSocket(6788);
+            byte[] buffer = new byte[1000];
+
+            while (true) {
+                DatagramPacket request = new DatagramPacket(buffer, buffer.length);
+                socket.receive(request);
+
+                String message = new String(request.getData(), 0, request.getLength());
+                System.out.println("Received from client: " + message);
+
+                byte[] sendMsg = (message.toUpperCase() + " from server to client").getBytes();
+                DatagramPacket reply = new DatagramPacket(sendMsg, sendMsg.length, request.getAddress(), request.getPort());
+                socket.send(reply);
+            }
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        } finally {
+            if (socket != null && !socket.isClosed()) {
+                socket.close();
+            }
+        }
+    }
 }
